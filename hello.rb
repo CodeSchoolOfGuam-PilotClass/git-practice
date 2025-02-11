6c6d57ecb8175f4918b4c5b8a11056a4203f8442
damenxweilbacher <weilbachers74@gmail.com>
2025-02-11 20:47:28 +1000

    get dunked on

--- hello.rb
+++ hello.rb
@@ -4,3 +4,11 @@ puts "SOUTH IN DA MOUTH"
 puts "Herro teriyaki hibachi benni hanna"
 puts "Hello, I'm Alanna."
 puts "hafa adai from noah"
+puts " ________
+    o      |   __   |
+      \_ O |  |__|  |
+   ____/ \ |___WW___|
+   __/   /     ||
+               ||
+               ||
+_______________||________________"
